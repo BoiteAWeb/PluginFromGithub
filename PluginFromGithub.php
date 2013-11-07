8e93651372ef705fefce819f48a35debb2b482b5
Julio Potier <juliobosk@gmail.com>
2013-11-07 23:57:29 +0100

    1st try

---
+++ PluginFromGithub.php
@@ -0,0 +1,9 @@
+/*
+Plugin Name: PluginFromGithub
+Author: Julio Potier
+*/
+
+add_action( 'wp_footer', '__wp_footer' );
+function __wp_footer(){
+	printf( '<p>%s</p>', date_i18n( 'l j F Y, H:i:s' ) );
+}
